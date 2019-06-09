fc3cddfffd89fa49da1366e29b71e10cd2ace5fc
ahmadvand <pegah.ahmadvand@gmail.com>
2019-06-09 10:54:53 +0430

    html camera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {Subject} from 'rxjs';
-import {Observable} from 'rxjs';
-import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
-
+import { Component } from '@angular/core';
 
 
 @Component({
@@ -11,73 +7,17 @@ import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
   styleUrls: ['./app.component.css']
 })
 
+export class AppComponent {
+  title = 'app';
+  selectedFile = null;
 
+  onFileSelected(event)
+  {
+    this.selectedFile = event.target.files[0];
+  }
 
-export class AppComponent implements OnInit {
-  title = 'firstangular';
- 
-  // toggle webcam on/off
- public showWebcam = true;
- public allowCameraSwitch = true;
- public multipleWebcamsAvailable = false;
- public deviceId: string;
- public videoOptions: MediaTrackConstraints = {
-   // width: {ideal: 1024},
-   // height: {ideal: 576}
- };
- public errors: WebcamInitError[] = [];
-
- // latest snapshot
- public webcamImage: WebcamImage = null;
-
- // webcam snapshot trigger
- private trigger: Subject<void> = new Subject<void>();
- // switch to next / previous / specific webcam; true/false: forward/backwards, string: deviceId
- private nextWebcam: Subject<boolean|string> = new Subject<boolean|string>();
-
- public ngOnInit(): void {
-   WebcamUtil.getAvailableVideoInputs()
-     .then((mediaDevices: MediaDeviceInfo[]) => {
-       this.multipleWebcamsAvailable = mediaDevices && mediaDevices.length > 1;
-     });
- }
-
- public triggerSnapshot(): void {
-   this.trigger.next();
- }
-
- public toggleWebcam(): void {
-   this.showWebcam = !this.showWebcam;
- }
-
- public handleInitError(error: WebcamInitError): void {
-   this.errors.push(error);
- }
-
- public showNextWebcam(directionOrDeviceId: boolean|string): void {
-   // true => move forward through devices
-   // false => move backwards through devices
-   // string => move to device with given deviceId
-   this.nextWebcam.next(directionOrDeviceId);
- }
-
- public handleImage(webcamImage: WebcamImage): void {
-   console.info('received webcam image', webcamImage);
-   this.webcamImage = webcamImage;
- }
-
- public cameraWasSwitched(deviceId: string): void {
-   console.log('active device: ' + deviceId);
-   this.deviceId = deviceId;
- }
-
- public get triggerObservable(): Observable<void> {
-   return this.trigger.asObservable();
- }
-
- public get nextWebcamObservable(): Observable<boolean|string> {
-   return this.nextWebcam.asObservable();
- }
-
+  onUpload()
+  {
+    console.log(this.selectedFile); // You can use FormData upload to backend server
+  }
 }
-
